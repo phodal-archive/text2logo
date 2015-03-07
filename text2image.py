72ee910f615a77bd76ad30b2771bbf5400214e49
Fengda HUANG <h@phodal.com>
2015-03-07 22:54:53 +0800

    use rgba color

--- text2image.py
+++ text2image.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 from PIL import Image, ImageDraw, ImageFont
 
+
 def add_corners(im, rad):
     circle = Image.new('L', (rad * 2, rad * 2), 0)
-    draw = ImageDraw.Draw(circle)
-    draw.ellipse((0, 0, rad * 2, rad * 2), fill=255)
+    image = ImageDraw.Draw(circle)
+    image.ellipse((0, 0, rad * 2, rad * 2), fill=255)
     alpha = Image.new('L', im.size, 255)
     w, h = im.size
     alpha.paste(circle.crop((0, 0, rad, rad)), (0, 0))
@@ -14,8 +15,10 @@ def add_corners(im, rad):
     im.putalpha(alpha)
     return im
 
-img = Image.new('L', (128, 128), 255)
+
+img = Image.new('RGBA', (128, 128), (255, 255, 255, 1))
 draw = ImageDraw.Draw(img)
+
 text_to_draw = unicode('自动化测试', 'utf-8')
 font = ImageFont.truetype('fonts/NotoSansCJKsc-Regular.otf', 24)
 draw.text((2, 50), text_to_draw, font=font)
