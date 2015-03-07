06faa635475c54933801460307c2e98b8e459573
Fengda HUANG <h@phodal.com>
2015-03-07 22:44:32 +0800

    add corners

--- text2image.py
+++ text2image.py
@@ -1,11 +1,24 @@
 # -*- coding: utf-8 -*-
 from PIL import Image, ImageDraw, ImageFont
 
+def add_corners(im, rad):
+    circle = Image.new('L', (rad * 2, rad * 2), 0)
+    draw = ImageDraw.Draw(circle)
+    draw.ellipse((0, 0, rad * 2, rad * 2), fill=255)
+    alpha = Image.new('L', im.size, 255)
+    w, h = im.size
+    alpha.paste(circle.crop((0, 0, rad, rad)), (0, 0))
+    alpha.paste(circle.crop((0, rad, rad, rad * 2)), (0, h - rad))
+    alpha.paste(circle.crop((rad, 0, rad * 2, rad)), (w - rad, 0))
+    alpha.paste(circle.crop((rad, rad, rad * 2, rad * 2)), (w - rad, h - rad))
+    im.putalpha(alpha)
+    return im
+
 img = Image.new('L', (128, 128), 255)
 draw = ImageDraw.Draw(img)
-text_to_draw = unicode('xxs', 'utf-8')
-font = ImageFont.truetype('fonts/NotoSansCJKsc-Regular.otf', 12)
-draw.text((2, 2), text_to_draw, font=font)
+text_to_draw = unicode('自动化测试', 'utf-8')
+font = ImageFont.truetype('fonts/NotoSansCJKsc-Regular.otf', 24)
+draw.text((2, 50), text_to_draw, font=font)
 del draw
-
+img = add_corners(img, 20)
 img.save('build/image.png')
